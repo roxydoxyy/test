769676f87ed76e0ec0863950a89600d092e97779
roxydoxyy <54900401+roxydoxyy@users.noreply.github.com>
2019-10-28 09:18:31 -0400

    Update assignment2.js

--- JS/assignment2.js
+++ JS/assignment2.js
@@ -2,10 +2,10 @@ for ( var i = 1; i <= 100; i++ ) {
   if (i%4 === 0 && i%10 === 0) {
     console.log( i + "TenFour" );
   }
-  else if (i%4 === 0 ) {
+  if (i%4 === 0 ) {
     console.log(i+ "Four" );
   }
-  else if (i%10 === 0 ) {
+  if (i%10 === 0 ) {
     console.log(i+ "Ten" );
   }
   else {
