7d79d2586bde6124e0e69c18ba4c1e9c18e4ee08
roxydoxyy <54900401+roxydoxyy@users.noreply.github.com>
2019-10-28 09:26:29 -0400

    Update assignment2.js

--- JS/assignment2.js
+++ JS/assignment2.js
@@ -1,19 +1,15 @@
-for ( var i = 1; i <= 100; i++ )
-{
-  if ( i%4 === 0 && i%10 === 0 )
-  {
-    console.log( i + " TenFour" );
-  }
-  else if ( i%4 === 0 ) 
-  {
-    console.log(i+ " Four" );
-  }
-  else if ( i%10 === 0 ) 
-  {
-    console.log(i+ " Ten" );
-  }
-  else
-  {
-    console.log(i);
-  }
-}
+function A1(){
+
+      var fName = prompt("Hey, what's your name?", name);
+      var myColour = prompt("Pick a colour, "+fName);
+
+      if (myColour == "red"){
+        console.log("Looks like this guy likes red. Noted")
+      } 
+
+      else if (myColour == "yellow"){
+        console.log("This guy chose yellow. Hmm")
+      } 
+
+      else {
+        console.log(fname+" chose a colour I hadn't thought of: "+ myColour)
